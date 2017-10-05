36b6eed1047bd2704c80433edd499295786d8108
fchristiansen <felipe@seo2.cl>
2017-10-05 18:34:22 -0300

    desarrollo html

--- include-form-codigo-desk.php
+++ include-form-codigo-desk.php
@@ -1,30 +1,34 @@
 <div class="box_codigo hidden-xs hidden-sm center-block">
 	<form id="form_codigo" class="form-inline">
-		<div class="row" >
-	    	<div class="input_codigo col-sm-4">
-		    	<div id="codigos">
-		        	<div class="form-group col-sm-8">
-		            	<input type="text" class="form-control" id="codigo" placeholder="CÓDIGO" name="codigo" required>
-		            </div>
-		            <div class="form-group col-sm-4" style="padding-left: 0;">
-		            	<input type="time" class="form-control" id="hora" placeholder="HH:MM" name="hora" required>
-		            </div>
-		    	</div>
-	        </div>
-	        <div class="col-sm-4">
-	        	<button type="submit" class="btn btn-default btn_amarillo" id="btnOK">ingresar</button>
-	        </div>
-	        <div class="col-sm-4">
-	        	<img src="assets/img/txt_debes_guardar.png" class="img-responsive">
-	        </div>			
-		</div>
-
-        
-        <div class="checkbox">
-        	<label>
-            	<input type="checkbox" id="leido"> He leído y acepto las bases legales 
-            </label>
+	    	<div class="col-sm-5">
+                <img class="img-responsive center-block" src="assets/img/txt-participa-aqui.png" alt="">
+                <div class="input_codigo">
+                        <div id="codigos">
+                            <div class="form-group col-sm-8">
+                                <input type="text" class="form-control" id="codigo" placeholder="CÓDIGO" name="codigo" required>
+                              </div>
+                              <div class="form-group col-sm-4" style="padding-left: 0;">
+                                <input type="time" class="form-control" id="hora" placeholder="HH:MM" name="hora" required>
+                              </div>
+                        </div>
+                </div>
+                <div class="checkbox">
+                       <label>
+                        <input type="checkbox" id="leido"> He leído y acepto las bases legales
+                       </label>
+                </div>
+	     </div>
+        <div class="col-sm-5">
+            <img class="img-responsive center-block" src="assets/img/txt-guardando-el-codigo.png" alt="">
             <div class="clear"></div>
+           <button type="submit" class="btn btn-default btn_amarillo" id="btnOK">ingresar</button>
+           <p class="nota">* <span class="texto-blanco">debes guardar tu envase</span> <span class="texto-amarillo">para hacer válido tu premio</span></p>
         </div>
+        <div class="col-sm-2">
+                <div id="badge">
+                    <img src="assets/img/badge.png" alt="">
+                </div>
+        </div>
+
     </form> <!-- form -->
 </div><!-- box codigo -->
--- index.php
+++ index.php
@@ -1,41 +1,74 @@
 <?php include('header.php') ?>
 <body class="body-offcanvas" id="home">
-	<div class="container" >
-		<div class="row">
-			<div class="col-sm-8 col-sm-offset-2">
-				<img src="assets/img/nuevo_llamado_desk.png" alt="" class="img-responsive center-block hidden-xs">
-				<img src="assets/img/mobile_header.png" alt="" class="img-responsive center-block visible-xs">
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-xs-10 col-xs-offset-1">
-				<div class="row">
-					<div class="col-xs-8 col-xs-offset-2 visible-xs">
-						<img src="assets/img/mobile_llamado.png" alt="" class="img-responsive center-block visible-xs">
+
+		<section class="call">
+			<div class="container">
+					<div class="row">
+						<div class="col-sm-8 col-sm-offset-2">
+							<img src="assets/img/nuevo_llamado_desk.png" alt="" class="img-responsive center-block hidden-xs">
+							<img src="assets/img/mobile_header.png" alt="" class="img-responsive center-block visible-xs">
+						</div>
 					</div>
-				</div>
+					<div class="row">
+						<div class="col-xs-10 col-xs-offset-1">
+							<div class="row">
+								<div class="col-xs-8 col-xs-offset-2 visible-xs">
+									<img src="assets/img/mobile_llamado.png" alt="" class="img-responsive center-block visible-xs">
+								</div>
+							</div>
+						</div>
+					</div>
 			</div>
-		</div>
+		</section>
+	<div class="container-fluid nopad">
+		<section class="box-formulario-codigo">
+				<div class="container">
+
+						<div class="row">
+						 <div class="" id="formcod">
+						      <?php
+						        if( $detect->isMobile() && !$detect->isTablet() ){
+						          // include('include-form-codigo-mobile.php');
+						      }else{
+						          //    include('include-form-codigo-desk.php');
+						      }
+						      ?>
+						      <?php
+						          //include('include-form-codigo-mobile.php');
+						          include('include-form-codigo-desk.php');
+						       ?>
+						  </div>
+						</div> <!-- row -->
+
+				</div>
+		</section>
+		<section class="box-fila">
+			<div class="container">
+
+				seccion fila
+
+			</div>
+		</section> <!-- box fila -->
+
 	</div>
 
-	<div class="container">
-		<div class="row">
-		 <div class="col-md-10 col-md-offset-1" id="formcod">
-				<?php
-						if( $detect->isMobile() && !$detect->isTablet() ){
-							 include('include-form-codigo-mobile.php');
-					}else{
-								include('include-form-codigo-desk.php');
-					}
-					?>
+	<div class="container nopad">
+		<section class="banner-bruno">
+				<img class="img-responsive center-block hidden-xs" src="assets/img/banner-bruno.png" alt="">
+				<img class="img-responsive center-block visible-xs" src="assets/img/banner-bruno-xs.png" alt="">
+		</section>
+
+	</div>
+
+
+
+		<section class="box-ingreso-datos">
+			<div class="container">
+					<div class="row">
+							<?php  include('include-form-ingreso-datos.php'); ?>
+					</div><!-- row -->
 			</div>
-		</div> <!-- row -->
-
-		<div class="row">
-				<?php  include('include-form-ingreso-datos.php'); ?>
-		</div><!-- row -->
-	</div> <!-- container -->
-
+		</section> <!-- box-ingreso-datos -->
 		<?php  include('include-modal-mensaje.php'); ?>
 		<?php  include('include-modal-instawin.php'); ?>
 		<?php include('footer.php') ?>
