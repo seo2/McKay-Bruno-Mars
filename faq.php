1e9a3edd49380cb5a14fbf9983d7ed82455b7dff
fchristiansen <felipe@seo2.cl>
2017-10-13 18:51:53 -0300

    update FAQ
    agregados nuevos textos a la seccion FAQ

--- faq.php
+++ faq.php
@@ -29,7 +29,7 @@
 								<li data-id="faq8">8.- ¿Debo llenar mis datos cada vez que participo?</li>
 								<li data-id="faq9">9.- ¿Cómo puedo ingresar al sitio de la promoción?</li>
 								<li data-id="faq10">10.- ¿Qué pasa si ingreso mal el código de fabricación?</li>
-								<li data-id="faq11">11.- ¿Cómo  gano?</li>
+								<li data-id="faq11">11.- ¿Cómo gano?</li>
 								<li data-id="faq12">12.- ¿Por qué piden tantos datos?</li>
 								<li data-id="faq13">13.- ¿Cómo sé si me están llamando de McKAY  y no es una mentira?</li>
 								<li data-id="faq14">14.- ¿Puedo cambiar el premio por dinero?</li>
@@ -49,7 +49,7 @@
 								<li data-id="faq8">8.- ¿Debo llenar mis datos cada vez que participo?</li>
 								<li data-id="faq9">9.- ¿Cómo puedo ingresar al sitio de la promoción?</li>
 								<li data-id="faq10">10.- ¿Qué pasa si ingreso mal el código de fabricación?</li>
-								<li data-id="faq11">11.- ¿Cómo  gano?</li>
+								<li data-id="faq11">11.- ¿Cómo gano?</li>
 								<li data-id="faq12">12.- ¿Por qué piden tantos datos?</li>
 								<li data-id="faq13">13.- ¿Cómo sé si me están llamando de McKAY  y no es una mentira?</li>
 								<li data-id="faq14">14.- ¿Puedo cambiar el premio por dinero?</li>
@@ -69,37 +69,38 @@
 									<div id="faq1" class="answer_faq">
 										<h4>1.- ¿Cuándo parte y cuándo termina la promoción?</h4>
 										<div class="box_faq">
-											<p>La Promoción se efectuará entre los días 01 de Junio de 2017 y 30 de Septiembre de 2017, ambas fechas inclusive.</p>
+											<p>La Promoción se efectuará entre los días 12 de octubre de 2017 y 19 de noviembre de 2017, ambas fechas inclusive.</p>
 										</div>
 									</div>
 
 									<div id="faq2" class="answer_faq hide">
 										<h4>2.- ¿Cómo puedo participar?</h4>
 										<div class="box_faq">
-											<p>Ingresa a <a href="https://www.mckay.cl">www.mckay.cl</a>, registra el código y hora de fabricación de tu galleta McKAY, a continuación, debes rellenar los campos con tus datos de contacto y ya estarás participando.</p>
+											<p>Ingresa a <a href="https://www.mckay.cl">www.mckay.cl</a> registra el código y hora de fabricación de tu galleta McKAY, a continuación, debes rellenar los campos con tus datos de contacto y ya estarás participando.</p>
+
 										</div>
 									</div>
 
 									<div id="faq3" class="answer_faq hide">
 										<h4>3.- ¿Dónde está el código y hora de fabricación?</h4>
 										<div class="box_faq">
-											<p>Cada producto tiene su código y hora de fabricación visible al reverso del envase (justo bajo la fecha de vencimiento). Busca en la página de la promoción la imagen explicativa, ahí se muestra claramente la ubicación del código.
- Ej: 42170077 H4 09:26 </p>
+
+											<p>Cada producto tiene su código y hora de fabricación visible al reverso del envase (justo bajo la fecha de vencimiento). Busca en la página de la promoción la imagen explicativa, ahí se muestra claramente la ubicación del código. Ej: 42170077 H4 09:26</p>
 										</div>
 									</div>
 
 									<div id="faq4" class="answer_faq hide">
 										<h4>4.- ¿Qué hago con el envase del producto participante?</h4>
 										<div class="box_faq">
-											<p>Debes conservar el envase de tus galletas McKAY ingresadas para poder validar tu participación. Si resultas ser el/la ganador/a , sin el envase no se podrá efectuar el canje del premio.</p>
+											<p>Debes conservar el envase de tus galletas McKAY ingresadas para poder validar tu participación. Si resultas ser él/la ganador/a, sin el envase no se podrá efectuar el canje del premio.</p>
 										</div>
 									</div>
 
 									<div id="faq5" class="answer_faq hide">
 										<h4>5.- ¿Qué productos participan?</h4>
 										<div class="box_faq">
-											<p>Participan todos los productos McKAY con y sin logo 125 años:</p>
-											<ul>
+											<p>Participan todos los productos McKAY.</p>
+											<!-- <ul>
 												<li>Criollitas</li>
 												<li>Alteza en todas sus variedades</li>
 												<li>Coco</li>
@@ -115,7 +116,7 @@
 												<li>Agua, Agua Light, Agua sin sal</li>
 												<li>Triton en todas sus variedades, incluido el Pack x3</li>
 												<li>Kuky en todas sus variedades</li>
-											</ul>
+											</ul> -->
 										</div>
 									</div>
 
@@ -129,47 +130,49 @@
 									<div id="faq7" class="answer_faq hide">
 										<h4>7.- ¿Qué pasa si tengo más de un envase con el mismo código?</h4>
 										<div class="box_faq">
-											<p>Técnicamente es posible que algunos empaques tengan el mismo código y hora de fabricación, por lo que está permitido que un mismo participante ingrese hasta 15 veces el mismo código y hora de fabricación. A partir del dieciseisavo (16º) código repetido con un mismo RUT, éstos serán eliminados de la base del sorteo. En caso que un participante resulte ganador de un premio con uno de estos códigos y hora de fabricación ingresado más de una vez, para poder cobrar el premio será necesario que guarde y presente un número de envases con ese código y hora de fabricación igual al número de veces que ingresó el mismo código y hora de fabricación. De lo contrario, el participante no podrá cobrar el premio.</p>
+											<p>Técnicamente, es posible que algunos empaques tengan el mismo código y hora de fabricación, por lo que está permitido que un mismo participante ingrese hasta 15 veces el mismo código y hora de fabricación. A partir del dieciseisavo (16º) código repetido con un mismo RUT, éstos serán eliminados de la base del sorteo. En caso que un participante resulte ganador de un premio con uno de estos códigos y hora de fabricación ingresado más de una vez, para poder cobrar el premio será necesario que guarde y presente un número de envases con ese código y hora de fabricación igual al número de veces que ingresó el mismo código y hora de fabricación. De lo contrario, el participante no podrá cobrar el premio.</p>
 										</div>
 									</div>
 
 									<div id="faq8" class="answer_faq hide">
 										<h4>8.- ¿Debo llenar mis datos cada vez que participo?</h4>
 										<div class="box_faq">
-											<p>No, si vas a ingresar más de un código, tus datos quedarán pre rellenados en los campos para que el ingreso de más códigos sea más fácil y rápido en <a href="https://www.mckay.cl">www.mckay.cl</a>.</p>
+											<p>No, si vas a ingresar más de un código, tus datos quedarán pre llenados en los campos para que el ingreso de más códigos sea más fácil y rápido en <a href="http://www.mckay.cl">www.mckay.cl</a> </p>
 										</div>
 									</div>
 
 									<div id="faq9" class="answer_faq hide">
 										<h4>9.- ¿Cómo puedo ingresar al sitio de la promoción?</h4>
 										<div class="box_faq">
-											<p>Podrás ingresar al sitio web promocional ingresando a  <a href="https://www.mckay.cl">www.mckay.cl</a>  a través de computadores, laptops, tablets y también a través de dispositivos móviles como celulares con conexión internet. Te recomendamos utilizar una red de conexión segura.</p>
+											<p>
+												Podrás ingresar al sitio web promocional ingresando a  <a href="https://www.mckay.cl">www.mckay.cl</a>  a través de computadores, laptops, tablets y también a través de dispositivos móviles como celulares con conexión internet. Te recomendamos utilizar una red de conexión segura.</p>
 										</div>
 									</div>
 
 									<div id="faq10" class="answer_faq hide">
 										<h4>10.- ¿Qué pasa si ingreso mal el código de fabricación?</h4>
 										<div class="box_faq">
-											<p>Si un participante ingresa un código de fabricación distinto al que se encuentra impreso en el producto, su participación no será válida, ya que el sistema no reconocerá datos. Si lo ingresaste mal, y no te lo aceptó, vuelve a ingresarlo correctamente. Si el problema persiste escríbenos en la sección “Contacto” que está en la web o llámanos al <a href="tel:+56224871400">+56 2 24871400</a>.</p>
+											<p>Si un participante ingresa un código de fabricación distinto al que se encuentra impreso en el producto, su participación no será válida, ya que el sistema no reconocerá datos. Si lo ingresaste mal, y no te lo aceptó, vuelve a ingresarlo correctamente. Si el problema persiste escríbenos en la sección “Contacto” del sitio web.</p>
 										</div>
 									</div>
 
 									<div id="faq11" class="answer_faq hide">
 										<h4>11.- ¿Cómo  gano?</h4>
 										<div class="box_faq">
-											<p>Debes ingresar el código de tus galletas McKAY junto a tus datos en <a href="https://www.mckay.cl">www.mckay.cl</a>, si eres ganador la producción de la promoción se contactará contigo. Mientras más códigos ingreses más posibilidades tienes de ganar. Es obligación mostrar tu envase ganador al momento de retirar tu premio, de no ser así, no será efectuada la entrega.</p>
+
+											<p>Debes ingresar el código de tus galletas McKAY junto a tus datos en <a href="https://www.mckay.cl">www.mckay.cl</a>, si eres ganador, la producción de la promoción se contactará contigo. Mientras más códigos ingreses más posibilidades tienes de ganar. Es obligación mostrar tu envase ganador al momento de retirar tu premio, de no ser así, no será efectuada la entrega.</p>
 										</div>
 									</div>
 
 									<div id="faq12" class="answer_faq hide">
 										<h4>12.- ¿Por qué piden tantos datos?</h4>
 										<div class="box_faq">
-											<p>Para contactarnos con la persona sorteada y poder verificar que realmente es el/la ganador/a. Por eso, al momento de ingresar los datos solicitados en el formulario, le aconsejamos revisar muy bien la información ingresada.</p>
+											<p>Para contactarnos con la persona sorteada y poder verificar que realmente es él/la ganador/a. Por eso, al momento de ingresar los datos solicitados en el formulario, le aconsejamos revisar muy bien la información ingresada.</p>
 										</div>
 									</div>
 
 									<div id="faq13" class="answer_faq hide">
-										<h4>13.- ¿Cómo sé si me están llamando de McKAY  y no es una mentira?</h4>
+										<h4>13.- ¿Cómo sé si me están llamando de McKAY y no es una mentira?</h4>
 										<div class="box_faq">
 											<p>Los concursantes sólo serán contactados por nuestras ejecutivas. Usted sólo deberá confirmar los datos ingresados en el formulario de participación y entregarnos el envase ganador. No debe entregar datos de cuenta corriente, no debe depositar ni entregar especies o dinero, ni debe tampoco entregar sus documentos.</p>
 										</div>
@@ -178,28 +181,29 @@
 									<div id="faq14" class="answer_faq hide">
 										<h4>14.- ¿Puedo cambiar el premio por dinero?</h4>
 										<div class="box_faq">
-											<p>Los premios de los sueños son siempre en dinero.<br>Los otros premios como, por ejemplo, galletas, galletero aniversario (no sueños) no son canjeables por dinero.</p>
+											<p>Las entradas no son canjeables por dinero.</p>
 										</div>
 									</div>
 
 									<div id="faq15" class="answer_faq hide">
 										<h4>15.- ¿Dónde puedo obtener más información?</h4>
 										<div class="box_faq">
-											<p>En el sitio web <a href="https://www.mckay.cl">www.mckay.cl</a> podrá encontrar toda la información sobre la promoción: bases legales, premios, como participar, formulario para ingresar datos, cómo buscar el código de fabricación y participar. Sugerimos leer las bases legales de la promoción. Si aun así tienes dudas puedes contactarnos a través de la opción contacto o a través de nuestro Fanpage oficial /GalletasMcKay o llamando  al <a href="tel:+56224871400">+56 2 2487 1400</a> o escribiendo a <a href="mailto:mckay@sinergika.cl">mckay@sinergika.cl</a></p>
+
+											<p>
+												En el sitio web <a href="https://www.mckay.cl">www.mckay.cl</a>  podrá encontrar toda la información sobre la promoción: bases legales, premios, como participar, formulario para ingresar datos, cómo buscar el código de fabricación y participar. Sugerimos leer las bases legales de la promoción. Si aun así tienes dudas puedes contactarnos a través de la opción contacto o a través de nuestro Fanpage oficial /GalletasMcKay o escribiendo a <a href="mailto:mckay@sinergika.cl">mckay@sinergika.cl</a>
+											</p>
 										</div>
 									</div>
 
 									<div id="faq16" class="answer_faq hide">
-										<h4>16.- ¿Cuántos premios regala McKay?</h4>
+										<h4>16.- ¿Cuántos premios regala McKay? </h4>
 										<div class="box_faq">
-											<p>En cumplimiento de lo dispuesto en el Artículo 36, de la Ley 19.496, sobre Protección de los  Derechos de los Consumidores, se deja constancia que esta Promoción contempla entregar los siguientes premios (cada uno, un “Premio”):</p>
+											<p>En cumplimiento de lo dispuesto en el Artículo 36, de la Ley 19.496, sobre Protección de los Derechos de los Consumidores, se deja constancia que esta Promoción contempla entregar los siguientes premios (cada uno, un “Premio”):</p>
 											<ul>
-												<li>100 “sueños” de $1.000.000 sorteados a través del ingreso de códigos en <a href="https://www.mckay.cl">www.mckay.cl</a>.</li>
-												<li>20 “sueños” de $1.000.000 a través de mecánicas digitales.</li>
-												<li>4 “sueños” de $2.000.000 que se seleccionarán por un panel interno.</li>
-												<li>1 gran “sueño” final de $25.000.000.</li>
-												<li>513 kits galletas</li>
-												<li>80 galleteros aniversario</li>
+												<li>105 entradas dobles al concierto de Bruno Mars en Chile a través del ingreso de códigos la Fila Virtual en <a href="https://www.mckay.cl">www.mckay.cl</a>. </li>
+													<li>20 entradas dobles al concierto de Bruno Mars en Chile a través de mecánicas digitales. </li>
+													<li>14 kits galletas.</li>
+													<li>14 galleteros aniversario.</li>
 											</ul>
 										</div>
 									</div>
